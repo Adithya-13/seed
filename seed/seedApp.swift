7e126900238ed170e069068005277ddb96671882
Adithya Firmansyah Putra <aditputrafirmansyah@gmail.com>
2026-01-18 20:14:10 +0700

    Initial Commit

---
+++ seed/seedApp.swift
@@ -0,0 +1,17 @@
+//
+//  seedApp.swift
+//  seed
+//
+//  Created by Adithya Firmansyah Putra on 18/01/26.
+//
+
+import SwiftUI
+
+@main
+struct seedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
